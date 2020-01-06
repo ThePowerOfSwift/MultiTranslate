d345b29c877c37931a7efea4d24ed7d8e43aeb7b
Keishin CHOU <keishin.chou@gmail.com>
2020-01-06 22:52:04 +0900

    Translation Layout.

--- MultiTranslate/AppDelegate.swift
+++ MultiTranslate/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+import IQKeyboardManager
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -15,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        IQKeyboardManager.shared().isEnabled = true
+        
         return true
     }
 
--- MultiTranslate/Controller/TranslationViewControllers/TextTranslateViewController.swift
+++ MultiTranslate/Controller/TranslationViewControllers/TextTranslateViewController.swift
@@ -8,34 +8,72 @@
 
 import UIKit
 
+import RAMAnimatedTabBarController
+
 class TextTranslateViewController: UIViewController {
+    
+    let exchangeButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "exchange"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: CGFloat(50)).isActive = true
+        button.widthAnchor.constraint(equalTo: button.heightAnchor, multiplier: 1).isActive = true
+        return button
+    }()
+    
+    override func loadView() {
+        super.loadView()
+        
+        let languageSelectView = UIView()
+        languageSelectView.backgroundColor = .red
+        
+        let sourceInputView = UIView()
+        sourceInputView.backgroundColor = .blue
+        
+        let targetOutputView = UIView()
+        targetOutputView.backgroundColor = .cyan
+        
+        let textTranslateStackView = UIStackView(arrangedSubviews: [languageSelectView, sourceInputView, targetOutputView])
+        textTranslateStackView.axis = .vertical
+        textTranslateStackView.distribution = .fillProportionally
+        
+        view.addSubview(textTranslateStackView)
+        textTranslateStackView.translatesAutoresizingMaskIntoConstraints = false
+        textTranslateStackView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        textTranslateStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        textTranslateStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        textTranslateStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        let sourceLanguageView = UIView()
+        sourceLanguageView.backgroundColor = .purple
+        
+        let exchangeButtonView = UIView()
+        exchangeButtonView.backgroundColor = .orange
+        
+        let targetLanguageView = UIView()
+        targetLanguageView.backgroundColor = .gray
+        
+        let languageSelectStackView = UIStackView(arrangedSubviews: [sourceLanguageView, exchangeButtonView, targetLanguageView])
+        languageSelectStackView.axis = .horizontal
+        languageSelectStackView.distribution = .fillProportionally
+        
+        languageSelectView.addSubview(languageSelectStackView)
+        languageSelectStackView.translatesAutoresizingMaskIntoConstraints = false
+        languageSelectStackView.topAnchor.constraint(equalTo: languageSelectView.topAnchor).isActive = true
+        languageSelectStackView.leadingAnchor.constraint(equalTo: languageSelectView.leadingAnchor).isActive = true
+        languageSelectStackView.trailingAnchor.constraint(equalTo: languageSelectView.trailingAnchor).isActive = true
+        languageSelectStackView.bottomAnchor.constraint(equalTo: languageSelectView.bottomAnchor).isActive = true
+        
+        exchangeButtonView.addSubview(exchangeButton)
+        exchangeButton.centerXAnchor.constraint(equalTo: exchangeButtonView.centerXAnchor).isActive = true
+        exchangeButton.centerYAnchor.constraint(equalTo: exchangeButtonView.centerYAnchor).isActive = true
+        
+
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        print("TextTranslateViewController here.")
-        
-        let label = UILabel(frame: .zero)
-        label.font = UIFont.systemFont(ofSize: 50, weight: UIFont.Weight.thin)
-        label.textColor = UIColor(red: 95/255, green: 102/255, blue: 108/255, alpha: 1)
-        label.text = "Text translation"
-        label.sizeToFit()
-        
-        view.addSubview(label)
-        view.constrainCentered(label)
-        view.backgroundColor = .white
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }

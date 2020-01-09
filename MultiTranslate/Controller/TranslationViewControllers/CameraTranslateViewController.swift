cca84b53487d23ea8613df9c96dfc2c46f5bc443
Keishin CHOU <keishin.chou@gmail.com>
2020-01-09 15:33:37 +0900

    Detect text in image.

--- MultiTranslate/Controller/TranslationViewControllers/CameraTranslateViewController.swift
+++ MultiTranslate/Controller/TranslationViewControllers/CameraTranslateViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import Vision
 
 import LBTATools
 import PMSuperButton
+import CropViewController
+import KRProgressHUD
 
 
 class CameraTranslateViewController: UIViewController {
+    
+    var detectedResultString = ""
     
     let container = UIView()
 
@@ -34,6 +39,22 @@ class CameraTranslateViewController: UIViewController {
     let paddingView1 = UIView(backgroundColor: .gray)
     let paddingView2 = UIView(backgroundColor: .brown)
     let buttonView = UIView(backgroundColor: .orange)
+    
+    let imagePicker: UIImagePickerController = {
+        let imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .camera
+        
+        return imagePicker
+    }()
+    
+    lazy private var textDetectionRequest: VNRecognizeTextRequest = {
+        let request = VNRecognizeTextRequest(completionHandler: self.handleDetectedText)
+        request.recognitionLevel = .accurate
+        request.recognitionLanguages = ["en_US"]
+        request.usesLanguageCorrection = true
+        return request
+    }()
     
     override func loadView() {
         super.loadView()
@@ -71,13 +92,84 @@ class CameraTranslateViewController: UIViewController {
         super.viewDidLoad()
 
         cameraImageContainer.addTarget(self, action: #selector(useCamera), for: .touchUpInside)
+        imagePicker.delegate = self
         
     }
     
     @objc func useCamera() {
         print("Use camera tapped.")
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera) {
+            present(imagePicker, animated: true, completion: nil)
+        } else {
+            print("Camera is not available.")
+        }
+        
+    }
+    
+    func handleDetectedText(request: VNRequest?, error: Error?) {
+        if error == nil {
+            guard let observations = request?.results as? [VNRecognizedTextObservation] else { return }
+            for observation in observations {
+                let bestObservation = observation.topCandidates(1).first
+                print(bestObservation?.string ?? "")
+                print(bestObservation?.confidence ?? 0)
+                guard let string = bestObservation?.string else { return }
+
+//                print(bestObservation?.boundingBox(for: Range<String.Index>))
+                detectedResultString += (string + " ")
+            }
+            
+//            let newViewController = ResultTableViewController()
+//            newViewController.observations = observations
+//            newViewController.modalPresentationStyle = .fullScreen
+//            self.navigationController?.pushViewController(newViewController, animated: true)
+            
+        }
     }
 
+}
 
+extension CameraTranslateViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate, CropViewControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
 
+        dismiss(animated: true) {
+            guard let image = info[.editedImage] as? UIImage else { return }
+
+            let cropViewController = CropViewController(image: image)
+            cropViewController.delegate = self
+            self.present(cropViewController, animated: true, completion: nil)
+        }
+    }
+    
+    func cropViewController(_ cropViewController: CropViewController, didCropToImage image: UIImage, withRect cropRect: CGRect, angle: Int) {
+        
+        KRProgressHUD.show()
+        
+        guard let cgImage = image.cgImage else { return }
+        let requests = [textDetectionRequest]
+        let imageRequestHandler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+//        DispatchQueue.global(qos: .userInitiated).async {
+        
+        do {
+            try imageRequestHandler.perform(requests)
+        } catch {
+            print("Process image error: \(error.localizedDescription)")
+        }
+        
+//        }
+        
+//        DispatchQueue.global(qos: .default).async(execute: { // time-consuming task
+        DispatchQueue.main.asyncAfter(deadline: .now()+1) {
+            KRProgressHUD.dismiss()
+        }
+        
+        dismiss(animated: true) {
+            let viewController = TextTranslateViewController()
+            viewController.sourceInputText.text = self.detectedResultString
+            self.present(viewController, animated: true, completion: nil)
+            
+        }
+    }
+    
 }

b50c13537499d428a41bfa3d0feedaa815ed4d52
Keishin CHOU <keishin.chou@gmail.com>
2020-02-17 21:19:20 +0900

    Change ConversationCell UI.

--- MultiTranslate/View/ConversationCell.swift
+++ MultiTranslate/View/ConversationCell.swift
@@ -10,8 +10,8 @@ import UIKit
 
 class ConversationCell: UITableViewCell {
     
-    var leadingConstraint: NSLayoutConstraint!
-    var trailingConstraint: NSLayoutConstraint!
+    private var leadingConstraints: [NSLayoutConstraint]!
+    private var trailingConstraints: [NSLayoutConstraint]!
 
     var conversation: Conversation? {
         didSet {
@@ -19,13 +19,29 @@ class ConversationCell: UITableViewCell {
             targetLabel.text = conversation?.targetMessage
             
             if conversation?.isSource == true {
-                bubbleBackgroundView.backgroundColor = .systemPink
-                leadingConstraint.isActive = true
-                trailingConstraint.isActive = false
+                bubbleBackgroundView.backgroundColor = UIColor(rgb: 0x40e0d0)
+                for eachConstraint in leadingConstraints {
+                    eachConstraint.isActive = true
+                }
+                for eachConstraint in trailingConstraints {
+                    eachConstraint.isActive = false
+                }
+                bubbleBackgroundView.layer.cornerRadius = 32
+                bubbleBackgroundView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner, .layerMaxXMaxYCorner]
+                sourceLabel.textAlignment = .left
+                targetLabel.textAlignment = .left
             } else {
-                bubbleBackgroundView.backgroundColor = .systemOrange
-                leadingConstraint.isActive = false
-                trailingConstraint.isActive = true
+                bubbleBackgroundView.backgroundColor = UIColor(rgb: 0xec7373)
+                for eachConstraint in trailingConstraints {
+                    eachConstraint.isActive = true
+                }
+                for eachConstraint in leadingConstraints {
+                    eachConstraint.isActive = false
+                }
+                bubbleBackgroundView.layer.cornerRadius = 32
+                bubbleBackgroundView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner, .layerMinXMaxYCorner]
+                sourceLabel.textAlignment = .right
+                targetLabel.textAlignment = .right
             }
         }
     }
@@ -33,8 +49,7 @@ class ConversationCell: UITableViewCell {
     private let bubbleBackgroundView: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .systemYellow
-        view.layer.cornerRadius = 5
+//        view.backgroundColor = .systemYellow
         
         return view
     }()
@@ -43,7 +58,7 @@ class ConversationCell: UITableViewCell {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.numberOfLines = 0
-        label.backgroundColor = .systemBlue
+//        label.backgroundColor = .systemBlue
         
         return label
     }()
@@ -52,14 +67,15 @@ class ConversationCell: UITableViewCell {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.numberOfLines = 0
-        label.backgroundColor = .systemIndigo
+//        label.backgroundColor = .systemIndigo
 
         return label
     }()
     
     private let deviderView: UIView = {
         let view = UIView()
-        view.backgroundColor = .systemBackground
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIColor(white: 0.9, alpha: 1)
 
         return view
     }()
@@ -68,25 +84,36 @@ class ConversationCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
+        self.backgroundColor = UIColor(rgb: 0xe1f2fb)
+        
         self.addSubview(bubbleBackgroundView)
         bubbleBackgroundView.topAnchor.constraint(equalTo: self.topAnchor, constant: 16).isActive = true
         bubbleBackgroundView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -16).isActive = true
-        bubbleBackgroundView.widthAnchor.constraint(lessThanOrEqualToConstant: 250).isActive = true
+        bubbleBackgroundView.widthAnchor.constraint(lessThanOrEqualToConstant: 350).isActive = true
+        bubbleBackgroundView.widthAnchor.constraint(greaterThanOrEqualToConstant: 150).isActive = true
+        bubbleBackgroundView.heightAnchor.constraint(greaterThanOrEqualToConstant: 64).isActive = true
         
-        bubbleBackgroundView.addSubview(sourceLabel)
-        sourceLabel.topAnchor.constraint(equalTo: bubbleBackgroundView.topAnchor, constant: 8).isActive = true
-        sourceLabel.leadingAnchor.constraint(equalTo: bubbleBackgroundView.leadingAnchor, constant: 8).isActive = true
-        sourceLabel.trailingAnchor.constraint(equalTo: bubbleBackgroundView.trailingAnchor, constant: -8).isActive = true
-        sourceLabel.bottomAnchor.constraint(equalTo: bubbleBackgroundView.centerYAnchor).isActive = true
+        bubbleBackgroundView.VStack(sourceLabel,
+                                    deviderView.setHeight(1),
+                                    targetLabel,
+                                    spacing: 0,
+                                    alignment: .fill,
+                                    distribution: .fill).padTop(8).padBottom(8)
         
-        bubbleBackgroundView.addSubview(targetLabel)
-        targetLabel.topAnchor.constraint(equalTo: bubbleBackgroundView.centerYAnchor).isActive = true
-        targetLabel.leadingAnchor.constraint(equalTo: sourceLabel.leadingAnchor).isActive = true
-        targetLabel.trailingAnchor.constraint(equalTo: sourceLabel.trailingAnchor).isActive = true
-        targetLabel.bottomAnchor.constraint(equalTo: bubbleBackgroundView.bottomAnchor, constant: -8).isActive = true
-        
-        leadingConstraint = bubbleBackgroundView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16)
-        trailingConstraint = bubbleBackgroundView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16)
+        leadingConstraints = [
+            bubbleBackgroundView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            sourceLabel.leadingAnchor.constraint(equalTo: bubbleBackgroundView.leadingAnchor, constant: 32),
+            sourceLabel.trailingAnchor.constraint(equalTo: bubbleBackgroundView.trailingAnchor, constant: -32),
+            targetLabel.leadingAnchor.constraint(equalTo: sourceLabel.leadingAnchor),
+            targetLabel.trailingAnchor.constraint(equalTo: sourceLabel.trailingAnchor)
+        ]
+        trailingConstraints = [
+            bubbleBackgroundView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+            sourceLabel.leadingAnchor.constraint(equalTo: bubbleBackgroundView.leadingAnchor, constant: 32),
+            sourceLabel.trailingAnchor.constraint(equalTo: bubbleBackgroundView.trailingAnchor, constant: -32),
+            targetLabel.leadingAnchor.constraint(equalTo: sourceLabel.leadingAnchor),
+            targetLabel.trailingAnchor.constraint(equalTo: sourceLabel.trailingAnchor)
+        ]
     }
     
     required init?(coder: NSCoder) {

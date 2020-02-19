33cef54cf803ba87a06f43c4df6ae0d1b9c09fb7
Keishin CHOU <keishin.chou@gmail.com>
2020-02-19 19:26:55 +0900

    Change AccountViewController UI.

--- MultiTranslate/Controller/AccountViewController.swift
+++ MultiTranslate/Controller/AccountViewController.swift
@@ -12,10 +12,90 @@ import SwiftyStoreKit
 
 class AccountViewController: UIViewController {
 
+    private var isYearlyPlan: Bool = true
+    private let colors = [UIColor(rgb: 0xff4d4d), UIColor(rgb: 0x46b3e6), UIColor(rgb: 0x738598)]
+    private let titles = ["No Limit", "50k characters/month", "10k characters/month"]
+    private let subTitles = ["For a pro translater", "Most popular", "Limited"]
+    private let illustrations = [UIImage(named: "crown"), UIImage(named: "firework"), UIImage(named: "diamond")]
+    
     private let container: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
-        
+        view.backgroundColor = UIColor(rgb: 0xC1D2EB)
+        return view
+    }()
+    
+    private let expressionContainerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let titleContainerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let imageContainerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let subtitleContainerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Get Multi-Translate PRO"
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 20.0)
+        return label
+    }()
+    
+    private let imageView: UIImageView = {
+        let view = UIImageView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.image = UIImage(named: "reading")
+        view.contentMode = .scaleAspectFill
+        return view
+    }()
+    
+    private let subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Multiple ways to translate with high accuracy!"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 17)
+        label.textColor = .systemGray
+        return label
+    }()
+    
+    private let selectorContainer: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let planSelector: UISegmentedControl = {
+        let selector = UISegmentedControl(items: ["Yearly plan", "Monthly plan"])
+        selector.translatesAutoresizingMaskIntoConstraints = false
+        selector.selectedSegmentIndex = 0
+//        selector.backgroundColor = UIColor(rgb: 0xe1f2fb)
+//        selector.selectedSegmentTintColor = UIColor(rgb: 0x4d80e4)
+        selector.layer.borderWidth = 1
+        selector.layer.borderColor = UIColor.white.cgColor
+        return selector
+    }()
+    
+    private let tableContainerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
     
@@ -35,11 +115,45 @@ class AccountViewController: UIViewController {
         container.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         container.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         
-        container.addSubview(subscritionPlanTableView)
-        subscritionPlanTableView.topAnchor.constraint(equalTo: container.topAnchor).isActive = true
-        subscritionPlanTableView.bottomAnchor.constraint(equalTo: container.bottomAnchor).isActive = true
-        subscritionPlanTableView.leadingAnchor.constraint(equalTo: container.leadingAnchor).isActive = true
-        subscritionPlanTableView.trailingAnchor.constraint(equalTo: container.trailingAnchor).isActive = true
+        container.VStack(expressionContainerView.setHeight(250),
+                         selectorContainer.setHeight(45),
+                         tableContainerView,
+                         spacing: 0,
+                         alignment: .fill,
+                         distribution: .fill).padTop(48)
+        
+        expressionContainerView.VStack(titleContainerView,
+                                       imageContainerView.setHeight(175),
+                                       subtitleContainerView,
+                                       spacing: 0,
+                                       alignment: .fill,
+                                       distribution: .fill)
+        
+        titleContainerView.heightAnchor.constraint(equalTo: subtitleContainerView.heightAnchor).isActive = true
+        
+        titleContainerView.addSubview(titleLabel)
+        titleLabel.centerInSuperview()
+        
+        imageContainerView.addSubview(imageView)
+        imageView.centerInSuperview()
+        imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor).isActive = true
+        imageView.topAnchor.constraint(equalTo: imageContainerView.topAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: imageContainerView.bottomAnchor).isActive = true
+        
+        subtitleContainerView.addSubview(subtitleLabel)
+        subtitleLabel.centerInSuperview()
+        
+        selectorContainer.addSubview(planSelector)
+        planSelector.topAnchor.constraint(equalTo: selectorContainer.topAnchor, constant: 15).isActive = true
+        planSelector.bottomAnchor.constraint(equalTo: selectorContainer.bottomAnchor).isActive = true
+        planSelector.leadingAnchor.constraint(equalTo: selectorContainer.leadingAnchor, constant: 88).isActive = true
+        planSelector.trailingAnchor.constraint(equalTo: selectorContainer.trailingAnchor, constant: -88).isActive = true
+
+        tableContainerView.addSubview(subscritionPlanTableView)
+        subscritionPlanTableView.topAnchor.constraint(equalTo: tableContainerView.topAnchor, constant: 32).isActive = true
+        subscritionPlanTableView.leadingAnchor.constraint(equalTo: tableContainerView.leadingAnchor).isActive = true
+        subscritionPlanTableView.bottomAnchor.constraint(equalTo: tableContainerView.bottomAnchor).isActive = true
+        subscritionPlanTableView.trailingAnchor.constraint(equalTo: tableContainerView.trailingAnchor).isActive = true
     }
 
 
@@ -51,11 +165,13 @@ class AccountViewController: UIViewController {
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Restore", style: .plain, target: self, action: #selector(restorePurchase))
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Purchase", style: .plain, target: self, action: #selector(purchaseTest))
         
+        planSelector.addTarget(self, action: #selector(planSelectorValueChanged(_:)), for: .valueChanged)
+        
         print(retrievedProducts)
         subscritionPlanTableView.dataSource = self
         subscritionPlanTableView.delegate = self
         
-        subscritionPlanTableView.register(UITableViewCell.self, forCellReuseIdentifier: Constants.subscritionPlanTableViewCellIdentifier)
+        subscritionPlanTableView.register(SubscritionPlanTableViewCell.self, forCellReuseIdentifier: Constants.subscritionPlanTableViewCellIdentifier)
         subscritionPlanTableView.separatorStyle = .none
     }
     
@@ -68,18 +184,62 @@ class AccountViewController: UIViewController {
         vc.modalPresentationStyle = .pageSheet
         present(vc, animated: true, completion: nil)
     }
+    
+    @objc func planSelectorValueChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            print("yearly plan")
+            isYearlyPlan = true
+            subscritionPlanTableView.reloadData()
+        case 1:
+            print("monthly plan")
+            isYearlyPlan = false
+            subscritionPlanTableView.reloadData()
+        default:
+            print("default plan...")
+        }
+    }
 
 }
 
 extension AccountViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return retrievedProducts.count
+        return 3
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.subscritionPlanTableViewCellIdentifier, for: indexPath)
-        cell.textLabel?.text = "Product: \(retrievedProducts[indexPath.row].localizedTitle),description: \(retrievedProducts[indexPath.row].localizedDescription), price: \(retrievedProducts[indexPath.row].localizedPrice!)"
-        cell.textLabel?.numberOfLines = 0
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.subscritionPlanTableViewCellIdentifier, for: indexPath) as! SubscritionPlanTableViewCell
+        
+        if isYearlyPlan {
+            cell.priceLabel.text = retrievedProducts[indexPath.row * 2].localizedPrice!
+            cell.priceExplanationLabel.text = "/year"
+        } else {
+            cell.priceLabel.text = retrievedProducts[indexPath.row * 2 + 1].localizedPrice!
+            cell.priceExplanationLabel.text = "/month"
+        }
+        
+        cell.illustration.image = illustrations[indexPath.row]
+        cell.titleLabel.text = titles[indexPath.row]
+        cell.subTitleLabel.text = subTitles[indexPath.row]
+        cell.container.backgroundColor = colors[indexPath.row]
+        
+        
+//        let colors = [UIColor(rgb: 0xff4d4d), UIColor(rgb: 0x46b3e6), UIColor(rgb: 0x738598)]
+        
+//        if isYearlyPlan {
+//            cell.textLabel?.text = "Product: \(retrievedProducts[indexPath.row * 2].localizedTitle),description: \(retrievedProducts[indexPath.row * 2].localizedDescription), price: \(retrievedProducts[indexPath.row * 2].localizedPrice!)"
+//        } else {
+//            cell.textLabel?.text = "Product: \(retrievedProducts[indexPath.row * 2 + 1].localizedTitle),description: \(retrievedProducts[indexPath.row * 2 + 1].localizedDescription), price: \(retrievedProducts[indexPath.row * 2 + 1].localizedPrice!)"
+//        }
+//
+//        cell.textLabel?.numberOfLines = 0
+//        cell.backgroundColor = colors[indexPath.row]
+        
+//        let layer = CAGradientLayer()
+//        layer.frame = cell.bounds
+//        layer.colors = [UIColor.red.cgColor, UIColor.black.cgColor]
+//        cell.layer.insertSublayer(layer, at: 0)
+        
         return cell
     }
     
---
+++ MultiTranslate/View/SubscritionPlanTableViewCell.swift
@@ -0,0 +1,131 @@
+//
+//  SubscritionPlanTableViewCell.swift
+//  MultiTranslate
+//
+//  Created by Keishin CHOU on 2020/02/19.
+//  Copyright © 2020 Keishin CHOU. All rights reserved.
+//
+
+import UIKit
+
+class SubscritionPlanTableViewCell: UITableViewCell {
+
+    let container: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let illustrationView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let titleView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let priceView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let illustration: UIImageView = {
+        let view = UIImageView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.contentMode = .scaleAspectFill
+        view.tintColor = .white
+        return view
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .natural
+        label.textColor = .white
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        label.sizeToFit()
+        return label
+    }()
+    
+    let subTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .natural
+        label.textColor = .systemGray3
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.sizeToFit()
+        return label
+    }()
+    
+    let priceLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .natural
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.sizeToFit()
+        return label
+    }()
+    
+    let priceExplanationLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .natural
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.sizeToFit()
+        return label
+    }()
+
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        self.addSubview(container)
+        container.topAnchor.constraint(equalTo: self.topAnchor, constant: 8).isActive = true
+        container.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -16).isActive = true
+        container.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 32).isActive = true
+        container.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -32).isActive = true
+        container.heightAnchor.constraint(equalToConstant: 64).isActive = true
+        container.layer.cornerRadius = 32
+        
+        container.hstack(illustrationView.setWidth(60),
+                         titleView,
+                         priceView.setWidth(75),
+                         spacing: 0,
+                         alignment: .fill,
+                         distribution: .fill)
+        
+        illustrationView.addSubview(illustration)
+        illustration.leadingAnchor.constraint(equalTo: illustrationView.leadingAnchor, constant: 16).isActive = true
+        illustration.centerYAnchor.constraint(equalTo: illustrationView.centerYAnchor).isActive = true
+        illustration.heightAnchor.constraint(equalToConstant: 36).isActive = true
+        illustration.widthAnchor.constraint(equalTo: illustration.heightAnchor).isActive = true
+        
+        titleView.addSubview(titleLabel)
+        titleLabel.leadingAnchor.constraint(equalTo: titleView.leadingAnchor, constant: 8).isActive = true
+        titleLabel.bottomAnchor.constraint(equalTo: titleView.centerYAnchor).isActive = true
+        
+        titleView.addSubview(subTitleLabel)
+        subTitleLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor).isActive = true
+        subTitleLabel.topAnchor.constraint(equalTo: titleView.centerYAnchor, constant: 8).isActive = true
+        
+        priceView.addSubview(priceLabel)
+        priceLabel.leadingAnchor.constraint(equalTo: priceView.leadingAnchor, constant: 8).isActive = true
+        priceLabel.bottomAnchor.constraint(equalTo: priceView.centerYAnchor).isActive = true
+        
+        priceView.addSubview(priceExplanationLabel)
+        priceExplanationLabel.leadingAnchor.constraint(equalTo: priceLabel.leadingAnchor).isActive = true
+        priceExplanationLabel.topAnchor.constraint(equalTo: priceView.centerYAnchor, constant: 8).isActive = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}

5b0de0e2efdbc6c0f212e3abe3af4bfae541e7ba
Keishin CHOU <keishin.chou@gmail.com>
2020-03-12 18:02:58 +0900

    Add dummy on-board page.

--- MultiTranslate/Controller/TranslationViewController.swift
+++ MultiTranslate/Controller/TranslationViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 import Parchment
+import paper_onboarding
 
 
 class TranslationViewController: UIViewController {
@@ -22,6 +23,20 @@ class TranslationViewController: UIViewController {
         "doc",
         "image",
     ]
+    
+    private let pageViewController: PagingViewController<IconItem> = {
+        let controller = PagingViewController<IconItem>()
+        controller.menuItemSource = .class(type: IconPagingCell.self)
+        controller.menuItemSize = .fixed(width: 70, height: 70)
+        controller.textColor = UIColor(red: 0.51, green: 0.54, blue: 0.56, alpha: 1)
+        controller.selectedTextColor = UIColor(red: 0.14, green: 0.77, blue: 0.85, alpha: 1)
+        controller.indicatorColor = UIColor(red: 0.14, green: 0.77, blue: 0.85, alpha: 1)
+        controller.menuBackgroundColor = UIColor.mtSystemBackground
+        controller.borderColor = .white
+        return controller
+    }()
+    
+    
     
     private lazy var textTranslateViewController = { () -> TextTranslateViewController in
         let viewController = TextTranslateViewController()
@@ -61,9 +76,60 @@ class TranslationViewController: UIViewController {
     
     var translationViewControllers = [UIViewController]()
     
+    private let onboardPage: PaperOnboarding = {
+        let view = PaperOnboarding()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let skipButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("skip", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .thin)
+        button.setTitleColor(UIColor.systemGray.adjustedAlpha(amount: 0.9), for: .normal)
+        return button
+    }()
+    
+    private let onboardItems = [
+        OnboardingItemInfo(informationImage: UIImage(systemName: "camera")!,
+                           title: "title",
+                           description: "description",
+                           pageIcon: UIImage(systemName: "camera")!,
+                           color: UIColor.systemPurple,
+                           titleColor: UIColor.label,
+                           descriptionColor: UIColor.label,
+                           titleFont: UIFont.preferredFont(forTextStyle: .title1),
+                           descriptionFont: UIFont.preferredFont(forTextStyle: .body)),
+
+        OnboardingItemInfo(informationImage: UIImage(systemName: "mic")!,
+                           title: "title",
+                           description: "description",
+                           pageIcon: UIImage(systemName: "mic")!,
+                           color: UIColor.systemTeal,
+                           titleColor: UIColor.label,
+                           descriptionColor: UIColor.label,
+                           titleFont: UIFont.preferredFont(forTextStyle: .title1),
+                           descriptionFont: UIFont.preferredFont(forTextStyle: .body)),
+
+        OnboardingItemInfo(informationImage: UIImage(systemName: "photo")!,
+                           title: "title",
+                           description: "description",
+                           pageIcon: UIImage(systemName: "photo")!,
+                           color: UIColor.systemOrange,
+                           titleColor: UIColor.label,
+                           descriptionColor: UIColor.label,
+                           titleFont: UIFont.preferredFont(forTextStyle: .title1),
+                           descriptionFont: UIFont.preferredFont(forTextStyle: .body)),
+    ]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        pageViewController.dataSource = self
+        pageViewController.select(pagingItem: IconItem(icon: icons[0], index: 0))
+        onboardPage.dataSource = self
         
         // Do any additional setup after loading the view.
         translationViewControllers = [
@@ -76,26 +142,6 @@ class TranslationViewController: UIViewController {
             imageTranslateViewController
         ]
         
-        let pagingViewController = PagingViewController<IconItem>()
-        pagingViewController.menuItemSource = .class(type: IconPagingCell.self)
-        pagingViewController.menuItemSize = .fixed(width: 70, height: 70)
-        pagingViewController.textColor = UIColor(red: 0.51, green: 0.54, blue: 0.56, alpha: 1)
-        pagingViewController.selectedTextColor = UIColor(red: 0.14, green: 0.77, blue: 0.85, alpha: 1)
-        pagingViewController.indicatorColor = UIColor(red: 0.14, green: 0.77, blue: 0.85, alpha: 1)
-        pagingViewController.dataSource = self
-        pagingViewController.select(pagingItem: IconItem(icon: icons[0], index: 0))
-//        pagingViewController.menuBackgroundColor = UIColor(rgb: 0xe1f2fb)
-        pagingViewController.menuBackgroundColor = UIColor.mtSystemBackground
-        pagingViewController.borderColor = .white
-        
-        // Add the paging view controller as a child view controller
-        // and contrain it to all edges.
-        addChild(pagingViewController)
-        view.addSubview(pagingViewController.view)
-        view.constrainToEdges(pagingViewController.view)
-        view.backgroundColor = .mtSystemBackground
-        pagingViewController.didMove(toParent: self)
-        
         NotificationCenter.default.addObserver(forName: .translationViewControllerDidChange,
                                                object: nil,
                                                queue: .main) { [weak self] (notification) in
@@ -106,10 +152,33 @@ class TranslationViewController: UIViewController {
             }
         }
         
+        skipButton.addTarget(self, action: #selector(skipOnboard), for: .touchUpInside)
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "tray.and.arrow.down"),
                                                             style: .plain,
                                                             target: self,
                                                             action: #selector(showDownloadLanguageModelPage))
+        
+        let isAppEverLaunched = UserDefaults.standard.bool(forKey: Constants.isAppEverLauchedKey)
+        if isAppEverLaunched {
+            addChild(pageViewController)
+            view.addSubview(pageViewController.view)
+            view.constrainToEdges(pageViewController.view)
+            view.backgroundColor = .mtSystemBackground
+            pageViewController.didMove(toParent: self)
+            
+        } else {
+            UserDefaults.standard.set(true, forKey: Constants.isAppEverLauchedKey)
+            view.addSubview(onboardPage)
+            onboardPage.edgeTo(view)
+            
+            view.addSubview(skipButton)
+            skipButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 35).isActive = true
+            skipButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive = true
+            
+            navigationController?.navigationBar.isHidden = true
+            tabBarController?.tabBar.isHidden = true
+        }
+        
     }
     
     @objc func showDownloadLanguageModelPage() {
@@ -138,12 +207,24 @@ class TranslationViewController: UIViewController {
             }
         }
     }
+    
+    @objc func skipOnboard() {
+        onboardPage.removeFromSuperview()
+        skipButton.removeFromSuperview()
+        
+        addChild(pageViewController)
+        view.addSubview(pageViewController.view)
+        view.constrainToEdges(pageViewController.view)
+        view.backgroundColor = .mtSystemBackground
+        pageViewController.didMove(toParent: self)
+        
+        navigationController?.navigationBar.isHidden = false
+        tabBarController?.tabBar.isHidden = false
+    }
 }
 
 extension TranslationViewController: PagingViewControllerDataSource {
-    
     func pagingViewController<T>(_ pagingViewController: PagingViewController<T>, viewControllerForIndex index: Int) -> UIViewController {
-//        return IconViewController(title: icons[index].capitalized)
         return translationViewControllers[index]
     }
     
@@ -154,5 +235,14 @@ extension TranslationViewController: PagingViewControllerDataSource {
     func numberOfViewControllers<T>(in: PagingViewController<T>) -> Int {
         return icons.count
     }
-    
+}
+
+extension TranslationViewController: PaperOnboardingDataSource {
+    func onboardingItem(at index: Int) -> OnboardingItemInfo {
+        return onboardItems[index]
+    }
+
+    func onboardingItemsCount() -> Int {
+        return onboardItems.count
+    }
 }
